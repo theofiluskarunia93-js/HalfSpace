264b76474615b37b68c9583b58b0ee0f72484d3e
theofiluskarunia93-js <theofiluskarunia93@gmail.com>
2026-06-14 12:42:13 +0700

    fix: remove sharp from serverExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["@resvg/resvg-js", "sharp"],
+  serverExternalPackages: ["@resvg/resvg-js"],
   images: {
     remotePatterns: [
       {
